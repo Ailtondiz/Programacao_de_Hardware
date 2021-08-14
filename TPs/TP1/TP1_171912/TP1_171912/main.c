2699b8a5b515e0916b84fc4ef972943d94489f4e
Ailton <ailtonfernandes_junior@hotmail.com>
2021-08-13 21:42:58 -0300

    TP1

---
+++ TPs/TP1/TP1_171912/TP1_171912/main.c
@@ -0,0 +1,20 @@
+/*
+ * TP1_171912.c
+ *
+ * Created: 13/08/2021 20:07:21
+ * Author : ailton
+ *	Adaptação do TP5
+ */ 
+
+#include <avr/io.h>
+extern void INICIO();
+
+int main(void)
+{
+    while (1) 
+    {
+		INICIO();
+
+    }
+}
+
